4dc2a6d8edcabeade72f0808e48de216772316ef
jacobneff <jacobleeneff@gmail.com>
2024-01-09 11:27:15 -0500

    added a commenting keymap for when in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,25 @@
-local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+-- line selection move
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- directory navigation
-keymap.set("n", "<leader>m", ":Oil<CR>", opts)
+vim.keymap.set("n", "<leader>m", ":Oil<CR>", opts)
 
 -- pane navigation
-keymap.set("n", "<C-h>", "<C-w>h", opts) -- move left
-keymap.set("n", "<C-j>", "<C-w>j", opts) -- move down
-keymap.set("n", "<C-k>", "<C-w>k", opts) -- move up
-keymap.set("n", "<C-l>", "<C-w>l", opts) -- move left
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts) -- move left
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts) -- move down
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts) -- move up
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts) -- move left
 
 -- indenting
-keymap.set("v", "<", "<gv")
-keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 -- comments
+
 vim.api.nvim_set_keymap("n", "<C-/>", "gcc", {noremap = false })
 vim.api.nvim_set_keymap("v", "<C-/>", "gcc", {noremap = false })
+vim.api.nvim_set_keymap("i", "<C-/>", "<C-c>gcc", {noremap = false })
+
