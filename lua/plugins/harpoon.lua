d664675a2c38118b647bd9abc179030ebd7392f6
jacobneff <jacobleeneff@gmail.com>
2024-01-30 16:02:13 -0500

    added harpoon functionality

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,79 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	lazy = "VeryLazy",
-	dependencies = { "nvim-lua/plenary.nvim" },
+  -- Harpoon plugin configuration
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    lazy = false,
+    requires = { "nvim-lua/plenary.nvim" }, -- if harpoon requires this
+    config = function()
+      require("harpoon").setup({})
+
+      local function toggle_telescope_with_harpoon(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = require("telescope.config").values.file_previewer({}),
+            sorter = require("telescope.config").values.generic_sorter({}),
+          })
+          :find()
+      end
+      vim.keymap.set("n", "<leader>H", function()
+        local harpoon = require("harpoon")
+        toggle_telescope_with_harpoon(harpoon:list())
+      end, { desc = "Open harpoon window" })
+    end,
+    keys = {
+      {
+        "<leader>A",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>n",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to file 1",
+      },
+      {
+        "<leader>e",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to file 2",
+      },
+      {
+        "<leader>i",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to file 3",
+      },
+      {
+        "<leader>o",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "harpoon to file 4",
+      },
+    },
+  },
 }
