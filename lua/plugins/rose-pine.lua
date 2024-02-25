0aff3f82bc0158c1975c4cf3cdcf23e4e854d4ea
jacobneff <jacobleeneff@gmail.com>
2024-02-25 13:32:35 -0500

    added files to config

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -4,6 +4,9 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+        require("rose-pine").setup({
+            -- disable_background = true;
+        })
 		vim.cmd("colorscheme rose-pine")
 	end,
 }
