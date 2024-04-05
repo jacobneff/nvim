f464c76758f5d7c8f66580ba8946f8e1e113b120
jacobneff <jacobleeneff@gmail.com>
2024-04-05 13:16:45 -0400

    updated trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,13 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open/close workspace diagnostics" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open/close document diagnostics" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open/close quickfix" },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open/close loclist" },
+    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open/close todo list" },
+  },
   -- opts - {},
 }
